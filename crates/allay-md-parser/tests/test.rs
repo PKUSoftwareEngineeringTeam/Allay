e50ee2ddfef582cefad93c5417a24c147540967f
LeoDreamer <2200010825@stu.pku.edu.cn>
2025-09-11 20:03:32 +0800

    [feat] API request for Markdown parser

---
+++ crates/allay-md-parser/tests/test.rs
@@ -0,0 +1,17 @@
+use allay_md_parser::AllayMdParser;
+
+#[test]
+fn test_basic() {
+    let markdown = "# Hello, world!\nThis is a new **test**.";
+    assert_eq!(
+        AllayMdParser::default().parse(markdown).trim(),
+        r#"<h1>Hello, world!</h1>
+<p>This is a new <strong>test</strong>.</p>"#
+    );
+}
+
+// #[test]
+// fn test_shortcode() {
+//     let shortcode = Shortcode::new("note".into(), "".into());
+//     let markdown = "{{ note }} This is a note. {{ /note }}";
+// }
