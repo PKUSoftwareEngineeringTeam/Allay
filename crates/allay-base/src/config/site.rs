d203f869584a2f6588315648330248e368d637b0
LeoDreamer <2200010825@stu.pku.edu.cn>
2025-09-13 19:50:58 +0800

    [fix] clippy

--- crates/allay-base/src/config/site.rs
+++ crates/allay-base/src/config/site.rs
@@ -13,17 +13,17 @@ description = "A brief description of your site."
 author = "Your Name"
 [params]"#;
 
-pub static SITE_CONFIG: LazyLock<AllayObject> = LazyLock::new(|| load_site_config());
+pub static SITE_CONFIG: LazyLock<AllayObject> = LazyLock::new(load_site_config);
 
 pub fn config_exists() -> bool {
     Path::new(CONFIG_FILE).exists()
 }
 
 fn load_site_config() -> AllayObject {
-    if let Ok(config) = read_file(workspace(CONFIG_FILE)) {
-        if let Ok(config) = AllayData::from_toml(&config.content) {
-            return config;
-        }
+    if let Ok(config) = read_file(workspace(CONFIG_FILE))
+        && let Ok(config) = AllayData::from_toml(&config.content)
+    {
+        return config;
     }
 
     AllayData::from_toml(DEFAULT_SITE_CONFIG).expect("Failed to parse default config")
