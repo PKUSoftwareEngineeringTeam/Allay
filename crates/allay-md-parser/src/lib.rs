6b4fdfec579fea55dbc9644f623c9bd285bf1ded
crl <crl18039102576@126.com>
2025-09-13 15:32:44 +0800

    fix typo and comment

--- crates/allay-md-parser/src/lib.rs
+++ crates/allay-md-parser/src/lib.rs
@@ -6,12 +6,12 @@ use std::{collections::HashMap, sync::LazyLock};
 use pulldown_cmark::{Parser, html};
 use regex::Regex;
 
-/// The type of a template variable
+/// The type of template variable
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum TemplateVariable {
     String(String), // should support string concat
     Integer(i64),   // should support integer operations (+, -, *, /, %)
-    Boolean(bool),  // should support boolean operations (&, |, !)
+    Boolean(bool),  // should support boolean operations (&&, ||, !)
     List(Vec<TemplateVariable>),
     Map(HashMap<String, TemplateVariable>),
     Null,
@@ -24,14 +24,12 @@ impl TemplateVariable {
     }
 }
 /// The regex pattern for commands
-pub static COMMAND_PATTERN: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r"\{-(\w+)-}").unwrap());
+pub static COMMAND_PATTERN: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\{-(\w+)-}").unwrap());
 /// The regex pattern for rendering an expression
-pub static RENDER_PATTERN: LazyLock<Regex> =
-    LazyLock::new(|| Regex::new(r"\{:(\w+):}").unwrap());
+pub static RENDER_PATTERN: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\{:(\w+):}").unwrap());
 
 /// Magic commands for template
-/// 
+///
 /// These are keywords for template engine to process the template
 /// Which means that user cannot use these keywords as variable names
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -47,7 +45,7 @@ pub enum TemplateCommand {
 
     /// Return a variable from current scope
     /// Can usually be omitted, as variables are automatically resolved
-    /// 
+    ///
     /// # Example:
     /// ```html
     /// {: get $var :}
@@ -104,9 +102,9 @@ pub enum TemplateCommand {
 
     /// Else block, to provide an alternative content for if block
     ///
-    /// # Example: 
+    /// # Example:
     /// ```html
-    /// {- if .is_admin -} 
+    /// {- if .is_admin -}
     /// <p>Admin</p>
     /// {- else -}
     /// <p>User</p>
@@ -119,7 +117,7 @@ pub enum TemplateCommand {
 
     /// Return the parameter passed to the scope, start from 0
     /// If the parameter does not exist, skip it
-    /// 
+    ///
     /// # Example:
     /// ```html
     /// <div class="say">{: param 0 :}</div>
@@ -131,13 +129,13 @@ pub enum TemplateCommand {
     /// The second parameter is the scope to pass to the included template
     /// If the scope is not provided, use the current scope
     /// Other parameters can be passed to the included template and accessed by PARAM
-    /// 
+    ///
     /// # Example:
     /// ```html
     /// {- include "header.html" -} <!-- use current scope -->
     /// {- include "article/post.html" .post "My Post" -} <!-- pass .post as scope, "My Post" as param 0 -->
     /// ```
-    INCLUDE
+    INCLUDE,
 }
 
 /// The variable scope for template, organized as a tree like json object
@@ -174,10 +172,10 @@ pub enum TemplateCommand {
 pub struct TemplateScope {}
 
 /// The root scope for template
-pub const GLOABL_SCOPE: TemplateScope = TemplateScope {};
+pub const GLOBAL_SCOPE: TemplateScope = TemplateScope {};
 
 /// The metadata for markdown, usually in YAML/TOML format
-/// It should totally configurable by user
+/// It should be totally configurable by user
 /// This content WILL BE RENDERED as HTML element if needed
 ///
 /// # Example
