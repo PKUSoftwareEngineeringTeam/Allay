6201410e4a1a6b3d8f63541c4911c3cfeac84646
LeoDreamer <110815661+LeoDreamer2004@users.noreply.github.com>
2025-09-28 21:15:01 +0800

    Rename backend to server (#52)
    * [fix] fix the article generation bug
    
    * [fix] delete extra debug
    
    * [rename] rename backend to server
    
    * [rename] rename server to web
    
    * [rename] rename server to web

--- crates/allay-web/src/server.rs
+++ crates/allay-web/src/server.rs
@@ -43,7 +43,7 @@ impl Server {
     ///
     /// # Examples
     /// ```
-    /// use allay_backend::server::Server;
+    /// use allay_web::server::Server;
     /// let server = Server::new("/path/to/directory", 8080, "localhost".to_string());
     /// ```
     pub fn new<P: AsRef<path::Path>>(path: P, port: u16, host: String) -> Self {
@@ -70,7 +70,7 @@ impl Server {
     /// # Examples
     ///
     /// ```ignore
-    /// use allay_backend::server::Server;
+    /// use allay_web::server::Server;
     /// let server = Server::new("/path/to/directory", 8080, "localhost".to_string());
     /// server.serve().unwrap();
     /// ```
@@ -118,7 +118,7 @@ impl Server {
             // TODO: Redirect to 404.html
             return Err((StatusCode::NOT_FOUND, "Not Found".to_string()));
         }
-        if path.strip_prefix(&*root).is_err() {
+        if path.strip_prefix(root.as_ref()).is_err() {
             return Err((StatusCode::FORBIDDEN, "Forbidden".to_string()));
         }
 
