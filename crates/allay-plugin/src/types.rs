4265ea9368176d8019c79767151de9e533b820f9
Runlin Chen <crl18039102576@126.com>
2025-11-20 20:23:06 +0800

    use better thread pool by rayon (#81)

--- crates/allay-plugin/src/types.rs
+++ crates/allay-plugin/src/types.rs
@@ -1,1 +1,9 @@
 pub use allay_plugin_host::{Header, Method, Request, Response};
+
+pub fn response_internal_error() -> Response {
+    Response {
+        status_code: 500,
+        headers: vec![],
+        body: Vec::new(),
+    }
+}
