c200c30d06b73db9a656c176ab0373b0c30b35f9
Runlin Chen <crl18039102576@126.com>
2025-11-20 20:23:06 +0800

    merge code

--- crates/allay-web/src/plugin_worker.rs
+++ crates/allay-web/src/plugin_worker.rs
@@ -1,79 +1,23 @@
 use allay_plugin::manager::Plugin;
-use allay_plugin::types::{Request, Response};
-use std::sync::Mutex;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::mpsc::{Receiver, Sender};
-use std::thread;
-
-struct WorkerThread {
-    join_handle: Option<thread::JoinHandle<()>>,
-    tx: Sender<Option<(Plugin, Request)>>,
-    result_rx: Receiver<Response>,
-}
-
-impl WorkerThread {
-    fn worker_loop(rx: Receiver<Option<(Plugin, Request)>>, result_tx: Sender<Response>) {
-        loop {
-            let payload = rx.recv().unwrap();
-            if let Some((plugin, request)) = payload {
-                let mut plugin = plugin.lock().expect("Failed to lock plugin");
-                let response = plugin.handle_request(request).expect("Failed to handle request");
-                result_tx.send(response).unwrap();
-            } else {
-                break;
-            }
-        }
-    }
-
-    fn new() -> Self {
-        let (tx, rx) = std::sync::mpsc::channel();
-        let (result_tx, result_rx) = std::sync::mpsc::channel();
-        let join_handle = thread::spawn(move || {
-            Self::worker_loop(rx, result_tx);
-        });
-        Self {
-            join_handle: Some(join_handle),
-            tx,
-            result_rx,
-        }
-    }
-
-    fn do_work(&self, plugin: Plugin, request: Request) -> Response {
-        assert!(self.join_handle.is_some(), "Worker thread is not running");
-        self.tx.send(Some((plugin, request))).unwrap();
-        self.result_rx.recv().unwrap()
-    }
-
-    fn stop(&mut self) {
-        self.tx.send(None).unwrap();
-        let join_handle = self.join_handle.take().unwrap();
-        join_handle.join().unwrap();
-    }
-}
-
-impl Drop for WorkerThread {
-    fn drop(&mut self) {
-        self.stop();
-    }
-}
+use allay_plugin::types::{Request, Response, response_internal_error};
 
 pub struct PluginWorker {
-    workers: Vec<Mutex<WorkerThread>>,
-    idx: AtomicUsize,
+    pool: rayon::ThreadPool,
 }
 
 impl PluginWorker {
     pub fn new(num_workers: usize) -> Self {
-        let workers = (0..num_workers).map(|_| Mutex::new(WorkerThread::new())).collect();
-        Self {
-            workers,
-            idx: AtomicUsize::new(0),
-        }
+        let pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(num_workers)
+            .build()
+            .expect("Failed to build thread pool");
+        Self { pool }
     }
 
     pub fn handle_request(&self, plugin: Plugin, request: Request) -> Response {
-        let idx = self.idx.fetch_add(1, Ordering::SeqCst) % self.workers.len();
-        let worker = self.workers[idx].lock().unwrap();
-        worker.do_work(plugin, request)
+        self.pool.install(move || {
+            let mut plugin = plugin.lock().expect("Failed to lock plugin");
+            plugin.handle_request(request).unwrap_or(response_internal_error())
+        })
     }
 }
